8f43906388175e2df64092688c4646afd240444e
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-08 21:25:16 +0900

    3を2回押した時に33になるようにした

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var number3:  Int = 3
-    var number4:  Int = 4
+    var number1:  Int = 0
+    var number2:  Int = 0
+    var number3:  Int = 0
+//    var number4:  Int = 4
+//    var number5:  Int = 5
+//    var number6:  Int = 6
+//    var number7:  Int = 7
+//    var number8:  Int = 8
+//    var number9:  Int = 9
+    
+    var plusButton:  UIButton!
+//    var minusButton:  UIButton!
+//    var multipeButton:  UIButton!
+//    var devideButton:  UIButton!
     
     var ope: Int = 0 //四則演算どれか判定
     
@@ -22,11 +34,44 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-    
+//    @IBAction func select1(){
+//        label.text = String(number1)
+//        number1 = number1 * 10 + 1
+//    }
+//    @IBAction func select2(){
+//        label.text = String(number2)
+//        number2 = number2 * 10 + 2
+//    }
     @IBAction func select3(){
-        label.text = String(number3)
-        
+        number1 = number1 * 10 + 3
+        label.text = String(number1)
+        number2 = number1
     }
+//    @IBAction func select4(){
+//        label.text = String(number4)
+//        number4 = number4 * 10 + 4
+//    }
+//    @IBAction func select5(){
+//        label.text = String(number5)
+//        number5 = number5 * 10 + 5
+//    }
+//    @IBAction func select6(){
+//        label.text = String(number6)
+//        number6 = number6 * 10 + 6
+//    }
+//    @IBAction func select7(){
+//        label.text = String(number7)
+//        number7 = number7 * 10 + 7
+//    }
+//    @IBAction func select8(){
+//        label.text = String(number8)
+//        number8 = number8 * 10 + 8
+//    }
+//    @IBAction func select9(){
+//        label.text = String(number9)
+//        number9 = number9 * 10 + 9
+//    }
+    
 
 
 }
