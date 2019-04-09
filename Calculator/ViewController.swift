f8147f043a771219719275601c9132434c711469
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-09 20:27:55 +0900

    0~9までの数字で足し算，引き算，掛け算，割り算をできるようにした

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,12 +13,7 @@ class ViewController: UIViewController {
     var number1:  Int = 0
     var number2:  Int = 0
     var number3:  Int = 0
-//    var number4:  Int = 4
-//    var number5:  Int = 5
-//    var number6:  Int = 6
-//    var number7:  Int = 7
-//    var number8:  Int = 8
-//    var number9:  Int = 9
+
     
     var plusButton:  UIButton!
 //    var minusButton:  UIButton!
@@ -34,43 +29,87 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-//    @IBAction func select1(){
-//        label.text = String(number1)
-//        number1 = number1 * 10 + 1
-//    }
-//    @IBAction func select2(){
-//        label.text = String(number2)
-//        number2 = number2 * 10 + 2
-//    }
+    @IBAction func plus(){
+        number2 = number1
+        number1 = 0
+        ope = 1
+    }
+    @IBAction func minus(){
+        number2 = number1
+        number1 = 0
+        ope = 2
+    }
+    @IBAction func multiple(){
+        number2 = number1
+        number1 = 0
+        ope = 3
+    }
+    @IBAction func devide(){
+        number2 = number1
+        number1 = 0
+        ope = 4
+    }
+    @IBAction func equal(){
+        if ope == 1 {
+            number3 = number2 + number1
+        } else if ope == 2 {
+            number3 = number2 - number1
+        } else if ope == 3 {
+            number3 = number2 * number1
+        } else if ope == 4 {
+            number3 = number2 / number1
+        }
+        label.text = String(number3)
+    }
+    @IBAction func clear(){
+        number1 = 0
+        number2 = 0
+        number3 = 0
+        label.text = String(number3)
+    }
+    
+    
+    @IBAction func select1(){
+        number1 = number1 * 10 + 1
+        label.text = String(number1)
+    }
+    @IBAction func select2(){
+        number1 = number1 * 10 + 2
+        label.text = String(number1)
+    }
     @IBAction func select3(){
         number1 = number1 * 10 + 3
         label.text = String(number1)
-        number2 = number1
     }
-//    @IBAction func select4(){
-//        label.text = String(number4)
-//        number4 = number4 * 10 + 4
-//    }
-//    @IBAction func select5(){
-//        label.text = String(number5)
-//        number5 = number5 * 10 + 5
-//    }
-//    @IBAction func select6(){
-//        label.text = String(number6)
-//        number6 = number6 * 10 + 6
-//    }
-//    @IBAction func select7(){
-//        label.text = String(number7)
-//        number7 = number7 * 10 + 7
-//    }
-//    @IBAction func select8(){
-//        label.text = String(number8)
-//        number8 = number8 * 10 + 8
-//    }
-//    @IBAction func select9(){
-//        label.text = String(number9)
-//        number9 = number9 * 10 + 9
-//    }
+    @IBAction func select4(){
+        number1 = number1 * 10 + 4
+        label.text = String(number1)
+    }
+    @IBAction func select5(){
+        number1 = number1 * 10 + 5
+        label.text = String(number1)
+    }
+    @IBAction func select6(){
+        number1 = number1 * 10 + 6
+        label.text = String(number1)
+    }
+    @IBAction func select7(){
+        number1 = number1 * 10 + 7
+        label.text = String(number1)
+    }
+    @IBAction func select8(){
+        number1 = number1 * 10 + 8
+        label.text = String(number1)
+    }
+    @IBAction func select9(){
+        number1 = number1 * 10 + 9
+        label.text = String(number1)
+    }
+    @IBAction func select0(){
+        number1 = number1 * 10 
+        label.text = String(number1)
+    }
+
     
 
 
